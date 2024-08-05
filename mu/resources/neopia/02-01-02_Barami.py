694b0fcd896471bcddf5826a236b4b10b7549e5f
Komron <knmv2311@gmail.com>
2024-08-05 14:31:20 +0500

    Updated keyboard example files

--- mu/resources/neopia/02-01-02_Barami.py
+++ mu/resources/neopia/02-01-02_Barami.py
@@ -3,13 +3,16 @@
 from neopia import *
 
 n =  Neosoco()
-while True:
-    key = Keyboard.read()
-    if key == '0':
+def on_press(key):
+    if Keyboard.key_to_str(key) == '0':
         n.motor_stop('right')
-    elif key == '1':
+    elif Keyboard.key_to_str(key) == '1':
         n.motor_rotate('right', 'forward', '10')
-    elif key == '2':
+    elif Keyboard.key_to_str(key) == '2':
         n.motor_rotate('right', 'forward', '50')
-    elif key == '3':
+    elif Keyboard.key_to_str(key) == '3':
         n.motor_rotate('right', 'forward', '100')
+    elif key == Keyboard.ESC:
+        return False
+    
+Keyboard.read(on_press)    
